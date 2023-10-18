643b8948c2300e5d6a993c3fae7dba00c3c32d8d
hukahirame <tokutaku2003@gmail.com>
2023-10-18 12:02:27 +0900

    ハンマーアニメーション追加

--- Assets/Hammer.cs
+++ Assets/Hammer.cs
@@ -4,26 +4,50 @@ using UnityEngine;
 
 public class Hammer : MonoBehaviour
 {
+    int i = 1;
+    int j = 1;
+
     void Start()
     {
-        Invoke("Strike", 2f);  
+
     }
 
-    /*private void Update()
+    void Update()
     {
-        if(Input.GetKey("e"))
-        {
-            Strike();
-        }
-    }*/
+        if ((Input.GetMouseButtonUp(0))&&(i+j ==2)) Strike();
+    }
     public void Strike() //アニメーション
     {
-        transform.rotation = Quaternion.Euler(0, 0, 115);
-        for (int i = 0; i < 20; i++)
-        {
-            transform.Rotate(0, 0, 2f * Time.deltaTime);
-            transform.rotation = Quaternion.Euler(0, 90, 115);
-        }
+        if (i == 1) transform.Rotate(0, 0, -90);
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y-4f, transform.eulerAngles.z);
 
+        i++;
+
+        if (i < 40)
+        {
+            Invoke("Strike", 0.015f);
+        }
+        else
+        {
+            i = 2; //一時終了
+            StrikeCooldown();
+        }
+    }
+    private void StrikeCooldown()
+    {
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y+4f, transform.eulerAngles.z);
+
+        j++;
+
+        if (j < 40)
+        {
+            Invoke("StrikeCooldown", 0.03f);
+        }
+        else
+        {
+            i = 1;
+            j = 1; //終了
+            transform.localEulerAngles = new Vector3(0, 0, 0);
+        }
     }
 }
