da8264ba9ec5daf251d1b07743d4873ff92cbe08
hamu_suke <castle89509@outlook.jp>
2023-10-28 17:57:38 +0900

    10/28 03

---
+++ Assets/GameManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public float time; //Œo‰ßŽžŠÔ
+
+    public static int op; //•s‹g“x
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        time += Time.deltaTime;
+    }
+}
